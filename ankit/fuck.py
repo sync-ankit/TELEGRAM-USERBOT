ebf33efb49813dcd4fb689af0fc7c6193a9a8247
/ A N K I T / <116625135+xnkit69@users.noreply.github.com>
2025-01-09 23:16:21 +0530

    Delete ankit directory

--- ankit/fuck.py
+++
@@ -1,7 +0,0 @@
-from telethon import events
-import ankit.client
-client = ankit.client.client
-
-@events.register(events.NewMessage(pattern=".fuck"))
-async def fuck(event):
-	await event.edit("┏━┳┳┳━┳┳┓\n┃━┫┃┃┏┫━┫┏┓\n┃┏┫┃┃┗┫┃┃┃┃\n┗┛┗━┻━┻┻┛┃┃\n┏┳┳━┳┳┳┓┏┫┣┳┓\n┣┓┃┃┃┃┣┫┃┏┻┻┫\n┃┃┃┃┃┃┃┃┣┻┫┃┃\n┗━┻━┻━┻┛┗━━━┛")
