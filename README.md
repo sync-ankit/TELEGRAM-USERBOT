2b2a4a4f7ccbd76fc4712eddfb94addc060aa08b
/ A N K I T / <116625135+xnkit69@users.noreply.github.com>
2025-01-09 23:07:42 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# Commands for install
+
+$ pkg update && pkg upgrade
+
+$ apt update && apt upgrade
+
+$ pkg install git
+
+$ pkg install python
+
+$ git clone https://github.com/sync-ankit/TG-UB
+
+$ cd TG-UB
+
+$ python setup.py
+
+$ python main.py
---
+++ setup.py
@@ -0,0 +1,13 @@
+import os
+os.system("pkg install pip")
+os.system("pip install telethon")
+os.system("pip install aiocron")
+os.system("pip install emoji")
+os.system("pip install asyncio")
+os.system("pip install datetime")
+os.system("pip install base64")
+os.system("pip install gpytranslate")
+os.system("pip install pytz")
+os.system("pip install gtts")
+os.system("pip install qrcode")
+os.system("clear")
